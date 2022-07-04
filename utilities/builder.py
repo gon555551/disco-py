09367fcaaaef41cbc3b79d69f7d28ec37f17dbcf
gon <gon.ven.nunes@gmail.com>
2022-07-04 01:53:47 +0100

    \n removed

--- utilities/builder.py
+++ utilities/builder.py
@@ -21,14 +21,12 @@ def message_obj(params: dict, id: str) -> dict:
             params["allowed_mentions"] = {"replied_user": False}
         else:
             params["allowed_mentions"] = None
-
     elif (
         params["allowed_mentions"] is not None
         and type(params["allowed_mentions"]) == bool
     ):
         params["message_reference"] = {"message_id": id}
         params["allowed_mentions"] = {"replied_user": params["allowed_mentions"]}
-
     else:
         params["message_reference"] = None
         params["allowed_mentions"] = None
